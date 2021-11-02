22a450a2f1875aa9399accf9420eede63b4a3392
Elias Trummer <trummer-linhard@gmx.at>
2021-11-02 17:12:22 +0100

    web sockets

---
+++ python_websockets/basic_ssl/server.py
@@ -0,0 +1,23 @@
+import asyncio
+import pathlib
+import ssl
+import websockets
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print(f"<<< {name}")
+
+    greeting = f"Hello {name}!"
+
+    await websocket.send(greeting)
+    print(f">>> {greeting}")
+
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+# localhost_pem = pathlib.Path(__file__).with_name("localhost.pem")
+ssl_context.load_cert_chain("cert.pem", "key.pem")
+
+async def main():
+    async with websockets.serve(hello, "localhost", 8765, ssl=ssl_context):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
